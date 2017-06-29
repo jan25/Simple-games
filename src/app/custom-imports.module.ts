8529a59ef4e81b23b19f6de15829a15003d98c60
jan25 <abhilashgnan@gmail.com>
2017-06-29 09:41:10 +0530

    tic tac toe basic impl with reset button on game over

--- src/app/custom-imports.module.ts
+++ src/app/custom-imports.module.ts
@@ -1,9 +1,18 @@
 import { NgModule } from '@angular/core';
 
+import {MdCardModule} from '@angular/material';
+import {MdGridListModule} from '@angular/material';
+import {MdButtonModule} from '@angular/material';
+import {MdIconModule} from '@angular/material';
+import {MdToolbarModule} from '@angular/material';
+import {MdTooltipModule} from '@angular/material';
+
 @NgModule({
 
-imports: [],
-exports: []
+imports: [MdCardModule, MdGridListModule, MdButtonModule,
+    MdIconModule, MdToolbarModule, MdTooltipModule],
+exports: [MdCardModule, MdGridListModule, MdButtonModule,
+    MdIconModule, MdToolbarModule, MdTooltipModule]
 
 })
 
---
+++ src/app/game-tile.class.ts
@@ -0,0 +1,8 @@
+
+export class GameTile {
+    used = false;
+    imgSrc = '';
+    mark = 0;
+    x = 0;
+    y = 0;
+}
